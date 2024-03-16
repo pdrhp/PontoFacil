5e6a1a1c465b8b7824b333c770a607c29f684c3f
Pedro Silva <pedrohsp2004@gmail.com>
2024-03-16 19:28:13 -0300

    feat: add middleware to validate JWT token

---
+++ src/middleware.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { ValidateToken } from './services/AuthService';
+
+interface TokenResponse {
+  flag: boolean;
+  message: string;
+  validToken: boolean;
+  token: string;
+}
+
+const BASEURL = 'http://localhost:3000/';
+ 
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/_next")) {
+    return NextResponse.next();
+    }
+
+  const cookie = cookies().get('token');
+
+  if(!cookie && request.nextUrl.pathname !== '/auth/login'){
+    return NextResponse.redirect(new URL('auth/login', BASEURL));
+  }
+
+  if(cookie){
+    const token = cookie.value;
+
+  try {
+    const response : TokenResponse = await ValidateToken(token);
+    if(response.validToken === false){
+      throw new Error('Invalid token');
+    }
+
+    if(response.validToken === true){
+      if(request.nextUrl.pathname === '/auth/login' 
+      || request.nextUrl.pathname === '/'){
+        return NextResponse.redirect(new URL('dashboard', request.nextUrl.origin));
+      }
+    }
+  }
+  catch(e){
+    return NextResponse.redirect(new URL('auth/login', request.url));
+  }
+  }
+  
+}
+ 
+export const config = {
+  matcher: ['/((?!auth/signup).*)'],
+}  
