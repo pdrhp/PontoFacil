0d544c0df74f4280b5b28270e636e2dd18d332bf
pedroo <pedrohsp2004@gmail.com>
2024-04-19 13:03:06 -0300

    Fix: fixed next.config.mjs and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,43 +11,43 @@ interface TokenResponse {
   token: string;
 }
 
-const BASEURL = 'http://localhost:3000/';
- 
+const BASEURL = process.env.BASE_URL;
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/_next")) {
     return NextResponse.next();
-    }
+  }
 
   const cookie = cookies().get('token');
 
-  if(!cookie && request.nextUrl.pathname !== '/auth/login'){
+  if (!cookie && request.nextUrl.pathname !== '/auth/login') {
     return NextResponse.redirect(new URL('auth/login', BASEURL));
   }
 
-  if(cookie){
+  if (cookie) {
     const token = cookie.value;
 
-  try {
-    const response : TokenResponse = await ValidateToken(token);
-    if(response.validToken === false){
-      throw new Error('Invalid token');
-    }
+    try {
+      const response: TokenResponse = await ValidateToken(token);
+      if (response.validToken === false) {
+        throw new Error('Invalid token');
+      }
 
-    if(response.validToken === true){
-      if(request.nextUrl.pathname === '/auth/login' 
-      || request.nextUrl.pathname === '/'){
-        return NextResponse.redirect(new URL('dashboard', request.nextUrl.origin));
+      if (response.validToken === true) {
+        if (request.nextUrl.pathname === '/auth/login'
+          || request.nextUrl.pathname === '/') {
+          return NextResponse.redirect(new URL('dashboard', request.nextUrl.origin));
+        }
       }
     }
+    catch (e) {
+      return NextResponse.redirect(new URL('auth/login', request.url));
+    }
   }
-  catch(e){
-    return NextResponse.redirect(new URL('auth/login', request.url));
-  }
-  }
-  
+
 }
- 
+
 export const config = {
   matcher: ['/((?!auth/signup).*)'],
 }  
