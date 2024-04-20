4e06c028820c2972ff8ad1a94cb7cc1394b1e208
Pedro <pedrohsp2004@gmail.com>
2024-04-20 12:57:23 -0300

    Fix: fixed middleware finally

--- src/middleware.ts
+++ src/middleware.ts
@@ -61,5 +61,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.(png|jpg|css|js)$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
