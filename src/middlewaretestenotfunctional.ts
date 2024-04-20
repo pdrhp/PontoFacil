27071f1246b2dfe6402a7e6639f815d09c0a9609
Pedro <pedrohsp2004@gmail.com>
2024-04-20 11:53:14 -0300

    Fix: removendo middleware para teste do carregamento de css

--- src/middlewaretestenotfunctional.ts
+++ src/middlewaretestenotfunctional.ts
@@ -61,5 +61,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.(png|jpg|css|js)$).*)'],
 }
